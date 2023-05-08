8b6e2e9fdc4a7984c004c1efef3fbf82f45d71a8
eugeneromancenko <eugene@romancenko.com>
2023-05-08 20:50:59 +0100

    timestamps app

--- timestamp/__init__.py
+++ timestamp/__init__.py
@@ -1,45 +1,62 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import boto3
 import os
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
+def create_app():
     """
-    Display a 'Timestamp' message.
+    Create and configure a Flask application instance.
     """
-    return "Timestamp app Ver1"
+    app = Flask(__name__)
 
-# Insert a timestamp into DynamoDB
-@app.route('/timestamp', methods=['POST'])
-def insert_timestamp():
-    # Get the timestamp from the request body
-    timestamp = request.json['timestamp']
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-    # Insert the timestamp into DynamoDB
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('timestamps')
-    response = table.put_item(
-        Item={
-            'timestamp': timestamp
-        }
-    )
+    @app.route("/")
+    def index():
+        """
+        Render the index page with buttons and a text field.
+        """
+        return render_template("index.html")
 
-    return jsonify(response)
+    @app.route('/timestamp', methods=['POST'])
+    def insert_timestamp():
+        # Get the timestamp from the request body
+        timestamp = request.form['timestamp']
 
-# List all timestamps from DynamoDB
-@app.route('/list')
-def list_timestamps():
-    # Get all items from DynamoDB
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('timestamps')
-    response = table.scan()
+        # Insert the timestamp into DynamoDB
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('timestamps')
+        response = table.put_item(
+            Item={
+                'timestamp': timestamp
+            }
+        )
 
-    # Extract the timestamps from the response
-    timestamps = [item['timestamp'] for item in response['Items']]
+        # Render the updated template with the response message
+        message = f"Timestamp {timestamp} added successfully"
+        return render_template('index.html', message=message)
 
-    return jsonify(timestamps)
+
+    @app.route('/list')
+    def list_timestamps():
+        # Get all items from DynamoDB
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('timestamps')
+        response = table.scan()
+
+        # Extract the timestamps from the response
+        timestamps = [item['timestamp'] for item in response['Items']]
+
+        # Render the updated template with the list of timestamps
+        return render_template('index.html', timestamps=timestamps)
+        
+
+    return app
+
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
